69bc45144eddd5efc9742ee985765a311e97dbe3
Chris Statzer <chris.statzer@gmail.com>
2024-03-06 10:59:42 +0800

    cleaned up readme formatting. Added tweens to the screen transitions

--- Data/Scripts/Camera.gd
+++ Data/Scripts/Camera.gd
@@ -3,17 +3,29 @@ extends Camera2D
 var screenWidth: int = ProjectSettings.get_setting("display/window/size/viewport_width")
 var screenHeight: int = ProjectSettings.get_setting("display/window/size/viewport_height")
 
+var screenSize: Vector2i = Vector2i(screenWidth, screenHeight)
+
 var player: CharacterBody2D
 
-# Called when the node enters the scene tree for the first time.
+var oldGridPosition: Vector2i = Vector2i(0, 0)
+var tweenDuration: int = 1000
+var tweenTimer: int
+
 func _ready():
 	player = $"../Player"
 	position = Vector2i(0, 0)
-	
+	oldGridPosition = Vector2i(player.position) / screenSize
 	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	var worldPosition = Vector2i(player.position) / Vector2i(screenWidth, screenHeight)
-	position = worldPosition * Vector2i(screenWidth, screenHeight)
+	var currentGridPosition = Vector2i(player.position) / screenSize
+
+	if currentGridPosition != oldGridPosition:
+		var moveTween = create_tween()
+		var toPosition = currentGridPosition * screenSize
+		var propTween = moveTween.tween_property(self, "position", Vector2(toPosition), 0.5)
+		oldGridPosition = currentGridPosition
+		print_debug("moving:", position, toPosition, moveTween, propTween)
+
+func _timer_timeout():
+	print_debug("Timer Timeout")
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 
-
 Resources
 =========
 
- * https://www.geeksforgeeks.org/complete-guide-on-2d-array-matrix-rotations/
+* <https://www.geeksforgeeks.org/complete-guide-on-2d-array-matrix-rotations/>
